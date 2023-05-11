96bde6a7782264949f44384330b79be76ccaedef
Eric Paul <ericcpaul00@gmail.com>
2023-05-11 23:19:31 +1000

    sooria helped

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,258 @@
-import Image from 'next/image'
+"use client";
+
+import { Field, FieldArray, FieldArrayItem, Form } from "houseform";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { TypeOf, z } from "zod";
+
+interface Milestone {
+  criteria: string[];
+}
+
+interface Group {
+  [key: string]: Milestone;
+}
+
+interface MarkingComments {
+  [key: string]: Group;
+}
+
+const fetchData = () =>
+  Promise.resolve({
+    Group1: {
+      milestone2: {
+        criteria: ["username/password/submit", "Login: User can login"],
+      },
+      milestone1: {
+        criteria: ["criteria1", "criteria2"],
+      },
+    },
+    // Group2: {
+    //   milestone3: {
+    //     criteria: ["username/password/submit", "Login: User can login"],
+    //   },
+    //   milestone4: {
+    //     criteria: ["Error: Erbutton.)"],
+    //   },
+    // },
+    // Group3: {
+    //   testing: {
+    //     criteria: ["username/password/submit", "Login: User can login"],
+    //   },
+    // },
+  });
+
+const initializeForm = (data: MarkingComments) => {
+  const initialValues: { [key: string]: string | number } = {};
+  const groupComments: { [key: string]: JSX.Element } = {};
+
+  return { initialValues, groupComments };
+};
+
+interface Criteria {
+  [criteria: string]: number;
+}
+
+interface Milestone {
+  [milestone: string]: Criteria;
+}
+
+interface Marks {
+  [group: string]: Milestone;
+}
+
+function getMarksFromFlattenedObject(data: Record<string, any>): Marks {
+  const marks: Marks = {};
+
+  // Process the flattened object
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      const value = data[key];
+
+      if (key.includes("_criteria_")) {
+        // Extract group, milestone, and criteria information from the key
+        const [group, rest] = key.split("_criteria_");
+        const [milestone, criteria] = rest.split("_");
+
+        // Create group object if it doesn't exist
+        if (!marks[group]) {
+          marks[group] = {};
+        }
+
+        // Create milestone object if it doesn't exist
+        if (!marks[group][milestone]) {
+          marks[group][milestone] = {};
+        }
+
+        // Assign the value to the criteria
+        marks[group][milestone][criteria] = value;
+      }
+    }
+  }
+
+  return marks;
+}
 
 export default function Home() {
+  const [data, setData] = useState<MarkingComments | null>(null);
+
+  useEffect(() => {
+    fetchData().then(setData);
+  }, []);
+
+  if (!data) {
+    return "Loading...";
+  }
+
+  const { initialValues, groupComments } = initializeForm(data);
+
+  console.log(initialValues, data);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="prose flex flex-col items-center justify-between p-24">
+      <Form
+        onSubmit={(values) => {
+          alert("Form was submitted with: " + JSON.stringify(values));
+          console.log(values);
+          console.log(transformData(values));
+        }}
+      >
+        {({ isValid, errors, submit, value, errorsMap }) => (
+          <form
+            className="w-[80%] flex flex-col gap-8"
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log("Submitting", value, errorsMap, isValid);
+
+              submit();
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            {Object.entries(data).map(([groupName, groupData], idx_group) => {
+              return (
+                <div key={groupName} className="card bg-red-500 p-5">
+                  <h1 className="text-xl font-bold">{groupName}</h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                  <FieldArray<{
+                    milestone_name: string;
+                    criteria: { criteria_name: string; mark: number }[];
+                  }>
+                    name={`${groupName}.milestones`}
+                    initialValue={Object.entries(groupData).map(
+                      ([milestone_name, milestone_data]) => ({
+                        milestone_name,
+                        criteria: milestone_data.criteria.map((c, index) => ({
+                          criteria_name: c,
+                          mark: 0,
+                          criteria_index: index,
+                        })),
+                      })
+                    )}
+                  >
+                    {({ value: milestone_vals }) => (
+                      <>
+                        {milestone_vals.map(
+                          ({ criteria, milestone_name }, idx_milestone) => (
+                            <FieldArrayItem<typeof criteria>
+                              key={milestone_name}
+                              name={`${groupName}.milestones[${idx_milestone}].criteria`}
+                              initialValue={criteria}
+                            >
+                              {({ value, setValue, onBlur, errors }) => (
+                                <div key={milestone_name}>
+                                  <h3 className="text-md font-semibold mt-4">
+                                    {milestone_name}
+                                  </h3>
+                                  <FieldArray<{
+                                    mark: number;
+                                    criteria_name: string;
+                                  }>
+                                    name={`${groupName}.milestones[${idx_milestone}].criteria.values`}
+                                    initialValue={value}
+                                  >
+                                    {({ value: values }) => (
+                                      <>
+                                        {values.map(
+                                          ({ criteria_name, mark }, index) => {
+                                            return (
+                                              <FieldArrayItem<number>
+                                                key={index}
+                                                name={`${groupName}.milestones[${idx_milestone}].criteria.values[${index}].mark`}
+                                                onSubmitValidate={z
+                                                  .number()
+                                                  .min(0, "dont even ask")}
+                                                initialValue={mark}
+                                              >
+                                                {({
+                                                  value,
+                                                  setValue,
+                                                  onBlur,
+                                                  errors,
+                                                }) => (
+                                                  <div>
+                                                    {criteria_name}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                                                    <br />
+                                                    <input
+                                                      value={value}
+                                                      onBlur={onBlur}
+                                                      onChange={(e) => {
+                                                        console.log(
+                                                          "im getting called"
+                                                        );
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                                                        setValue(
+                                                          Number(e.target.value)
+                                                        );
+                                                      }}
+                                                      placeholder={
+                                                        criteria_name
+                                                      }
+                                                      type="number"
+                                                    />
+                                                    {errors.map((error) => (
+                                                      <p key={error}>{error}</p>
+                                                    ))}
+                                                  </div>
+                                                )}
+                                              </FieldArrayItem>
+                                            );
+                                          }
+                                        )}
+                                      </>
+                                    )}
+                                  </FieldArray>
+                                </div>
+                              )}
+                            </FieldArrayItem>
+                          )
+                        )}
+                      </>
+                    )}
+                  </FieldArray>
+                  <h1 className="text-xl font-bold mb-2 mt-6">Comments</h1>
+                  <Field name={`${groupName}.comments`}>
+                    {({ value, setValue, onBlur, errors }) => (
+                      <div>
+                        <input
+                          className="textarea w-full min-h-16"
+                          value={value}
+                          onBlur={onBlur}
+                          onChange={(e) => setValue(e.target.value)}
+                          placeholder={groupName}
+                        />
+                        {errors.map((error) => (
+                          <p key={error}>{error}</p>
+                        ))}
+                      </div>
+                    )}
+                  </Field>
+                </div>
+              );
+            })}
+            <button type="submit">Submit</button>
+          </form>
+        )}
+      </Form>
     </main>
-  )
+  );
 }
