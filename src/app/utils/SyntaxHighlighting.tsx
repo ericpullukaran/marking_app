2fe7e072caa032728268917b763d3ccda2d2ae6a
Soorria Saruva <soorria.ss@gmail.com>
2023-06-19 18:21:22 +1000

    make code block prettier w/ animation and border radius change

--- src/app/utils/SyntaxHighlighting.tsx
+++ src/app/utils/SyntaxHighlighting.tsx
@@ -1,10 +1,47 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 import { useCopy } from "./useCopy";
 import { visit } from "unist-util-visit";
 import { useHydrated } from "./use-hydrated";
 import { saveAs } from "file-saver";
+import { cx } from "./cs";
+
+/**
+ * Shows `bottom` when `toggled` is true, otherwise shows `top`.
+ *
+ * Yoinked and slightly changed from https://github.com/soorria/soorria.com/blob/main/src/components/mdx/utils.tsx#L68-L92
+ */
+const ToggleSlide = ({
+  top,
+  bottom,
+  toggled,
+}: {
+  top: ReactNode;
+  bottom: ReactNode;
+  toggled: boolean;
+}) => {
+  return (
+    <span className="inline-grid overflow-hidden">
+      <span
+        className={cx(
+          "col-start-1 row-start-1 transition-transform",
+          toggled && "-translate-y-full"
+        )}
+      >
+        {top}
+      </span>
+      <span
+        className={cx(
+          "col-start-1 row-start-1 transition-transform",
+          !toggled && "translate-y-full"
+        )}
+      >
+        {bottom}
+      </span>
+    </span>
+  );
+};
 
 export default function SyntaxHighlight({
   className,
@@ -12,7 +49,7 @@ export default function SyntaxHighlight({
   ...rest
 }: {
   className: string;
-  children: string;
+  children: ReactNode;
 }) {
   const [copy, copied] = useCopy();
   const [download, downloaded] = useCopy();
@@ -79,45 +116,49 @@ export default function SyntaxHighlight({
             onClick={handleCopyClick}
             className="btn btn-sm btn-outline btn-accent p-2 rounded-lg"
           >
-            {copied ? "Copied!" : "Copy"}
+            <ToggleSlide top="Copied!" bottom="Copy" toggled={!copied} />
           </button>
           {hasTitle && (
             <button
               onClick={handleDownloadClick}
               className="btn btn-sm btn-accent rounded-lg"
             >
-              {downloaded ? (
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={1.5}
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={1.5}
-                  xmlns="http://www.w3.org/2000/svg"
-                  aria-hidden="true"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-                  />
-                </svg>
-              )}
+              <ToggleSlide
+                top={
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={1.5}
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                }
+                bottom={
+                  <svg
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={1.5}
+                    xmlns="http://www.w3.org/2000/svg"
+                    aria-hidden="true"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                    />
+                  </svg>
+                }
+                toggled={!downloaded}
+              />
             </button>
           )}
         </div>
@@ -126,7 +167,7 @@ export default function SyntaxHighlight({
       <div className="relative">
         <pre
           ref={pre}
-          className={`bg-[#1B1D20] py-4 rounded-xl ${
+          className={`bg-[#1B1D20] py-4 rounded-md ${
             showFullCode ? "" : "h-32 overflow-scroll"
           }`}
         >
