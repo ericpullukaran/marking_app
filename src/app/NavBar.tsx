2cf90c5062285bd2eaefd6dff0efba854d496d4b
Soorria Saruva <soorria.ss@gmail.com>
2023-06-19 11:11:42 +1000

    use spa links, and add blank .prettierrc for consistency

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,22 +1,23 @@
-import React from "react";
+import React from 'react'
+import Link from 'next/link'
 
-type Props = {};
+type Props = {}
 
 export default function NavBar({}: Props) {
   return (
     <div className="navbar h-20 bg-[#26262A] ">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl " href="/">
+        <Link className="btn btn-ghost normal-case text-xl" href="/">
           CS6080 - Marking App
-        </a>
+        </Link>
       </div>
 
-      <a
+      <Link
         href="/docs"
         className="flex-none btn bg-[#26262A] border border-[#4D4D4D] border-opacity-40 mr-4"
       >
         Docs
-      </a>
+      </Link>
     </div>
-  );
+  )
 }
